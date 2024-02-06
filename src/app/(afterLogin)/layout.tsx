9a2a1e40f64b1816d11d990862ca18a98b9df654
chaechae66 <valueworkerchae@gmail.com>
2024-02-06 21:02:46 +0900

    Feat: 로그인 후 화면에서 사용자 정보 나오게만 구현

---
+++ src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,48 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function afterLoginLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/");
+  }
+  return (
+    <>
+      <header className="sticky top-0 flex h-16 items-center justify-between border-b-[1px] border-solid border-gray-200 bg-white px-4">
+        <Link href={"/home"} className="flex text-2xl font-bold text-primary">
+          <Image src={"/logo.png"} alt="메인로고" width={40} height={40} />
+          <h2 className="ml-2">하우스,톡</h2>
+        </Link>
+        <div>
+          {session.user?.image ? (
+            <Image
+              className="h-12 w-12 rounded-full"
+              src={session.user?.image}
+              alt={session.user?.name!}
+              width={40}
+              height={40}
+            />
+          ) : (
+            <div className="flex-center h-12 w-12 rounded-full bg-slate-400">
+              <Image
+                src="/no_image.svg"
+                alt="프로필사진 없음"
+                width={20}
+                height={20}
+              />
+            </div>
+          )}
+        </div>
+      </header>
+      <main className="h-[calc(100%-theme(space.36))] w-full">{children}</main>
+    </>
+  );
+}
---
+++ src/app/(beforeLogin)/login/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import KakaoLogin from "../_components/kakaoLogin";
+import NaverLogin from "../_components/NaverLogin";
+
+export default function Login() {
+  return (
+    <div className="flex-center h-full w-full flex-col">
+      <h2 className="mb-12 text-3xl font-medium">로그인</h2>
+      <div className="w-80">
+        <form className="w-full">
+          <input
+            placeholder="이메일"
+            type="text"
+            className="mb-4 w-full rounded border-[1px] border-solid border-gray-200 p-2"
+          />
+          <br />
+          <input
+            placeholder="비밀번호"
+            type="password"
+            className="mb-8 w-full rounded border-[1px] border-solid border-gray-200 p-2"
+          />
+          <button
+            type="submit"
+            className="w-full rounded bg-primary p-2 text-center text-white"
+          >
+            로그인
+          </button>
+        </form>
+        <div className="mb-4 mt-10 h-0.5 w-full bg-gray-200"></div>
+        <h4 className="mb-4 w-full text-center">소셜로그인 이용하기</h4>
+        <div className="flex-center">
+          <NaverLogin />
+          <KakaoLogin />
+        </div>
+      </div>
+    </div>
+  );
+}
