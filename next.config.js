fede9a218fffb420f0ed3fc31b6c5faf4c5acaf8
chaechae66 <valueworkerchae@gmail.com>
2024-02-06 20:59:20 +0900

    Feat: Nextjs에서 카카오 이미지 쓸 수 있도록 config에 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
